d491bc97f794bd79a78e71e76470b66f048584a4
Sourcery AI <>
2023-08-03 08:11:50 +0000

    'Refactored by Sourcery'

--- pyShop/models.py
+++ pyShop/models.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 class CustomUserManager(UserManager):
     def get_by_natural_key(self, username):
-        case_insensitive_username_field = '{}__iexact'.format(
-            self.model.USERNAME_FIELD)
+        case_insensitive_username_field = f'{self.model.USERNAME_FIELD}__iexact'
         return self.get(**{case_insensitive_username_field: username})
 
 
